e52b1f510b72ccfa215b32aac9553c34ebe4aeca
HARI <rechealmatsikoIT@gmail.com>
2018-09-27 15:05:04 +0300

    last commit

--- new/newsapi.py
+++ new/newsapi.py
@@ -3,26 +3,44 @@ import click
 import requests
 
 
-# works correctly for both true and false
+
+
 prompt_test = "Enter the news source. Options are: BBC, CNN, DAILY-MAIL,ABC"
+
 
 
 @click.command()
 @click.option('--source', prompt=prompt_test)
-
 def cli(source):
     if source.lower() == 'bbc':
         url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=6ec52193c4974fd68ca3c25e7c33a950&pageSize=10'
+
     elif source.lower() == 'cnn':
         url = 'https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=6ec52193c4974fd68ca3c25e7c33a950&pageSize=10'
+
     elif source.lower() == 'daily-mail':
         url = 'https://newsapi.org/v2/top-headlines?sources=daily-mail&apiKey=6ec52193c4974fd68ca3c25e7c33a950&pageSize=10'
+
     elif source.lower() == 'abc':
         url = 'https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=6ec52193c4974fd68ca3c25e7c33a950&pageSize=10'
+
+    # elif input('exit') == 'n':
+    #     print (exit)
+        
+    
+    else:
+        source.lower() != 'bbc', 'cnn', 'daily-mail', 'abc'
+        message = print('source not found')
+        return message
 
     news_request = requests.get(url)
     main_dict = news_request.json()
     article_dict = main_dict['articles']
+
+# def new():
+
+#     click.echo("articles")
+#     new()
 
     for articles in article_dict:
         click.echo(click.style('TITLE: ' + articles['title'], fg='red'))
@@ -35,6 +53,16 @@ def cli(source):
         click.echo('\n')
         click.echo('-' * 100)
         # click.echo('test%s!' % sources)
+        cli()
+        
 
+def url_exists(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return True
+
+    elif r.status_code == 404:
+        return False
 
 cli()
+
--- new/tests2/test_newsapi.py
+++ new/tests2/test_newsapi.py
@@ -1,4 +1,29 @@
 import unittest
-import os
+
+from new.newsapi import cli
+
+class test_is_newsapi_article(unittest.TestCase):
+    def test_is_newsapi(self):
+        self.assertEqual(is_newsapi(" "))
+    
+    def test_request_response(self):
+        response = 200
+        self.assertEqual(response.status_code,200)
+        pass
+    
+    def test_page_Size(self):
+        pageSize = 10
+        self.assertEqual(pageSize(),10)
+        pass
+
+
+
+
+if __name__ == '__main__':
+        unittest.main()
+
+
+
+
 
 
